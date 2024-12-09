f0ad4d63975fd30c89043787f4939c180bf745e4
vipabh2 <a8919832@googlemail.com>
2024-12-10 02:57:19 +0300

    Update secret.py

--- VIPABH/assistant/secret.py
+++ VIPABH/assistant/secret.py
@@ -4,18 +4,18 @@ import re
 
 from telethon.events import CallbackQuery
 
-from JoKeRUB import l313l
+from VIPABH import ABH
 
 
-@l313l.tgbot.on(CallbackQuery(data=re.compile(b"rzan_(.*)")))
+@ABH.tgbot.on(CallbackQuery(data=re.compile(b"rzan_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
-    if os.path.exists("./JoKeRUB/secrets.txt"):
-        jsondata = json.load(open("./JoKeRUB/secrets.txt"))
+    if os.path.exists("./VIPABH/secrets.txt"):
+        jsondata = json.load(open("./VIPABH/secrets.txt"))
         try:
             message = jsondata[f"{timestamp}"]
             userid = message["userid"]
-            ids = [userid, l313l.uid]
+            ids = [userid, ABH.uid]
             if event.query.user_id in ids:
                 encrypted_tcxt = message["text"]
                 reply_pop_up_alert = encrypted_tcxt
