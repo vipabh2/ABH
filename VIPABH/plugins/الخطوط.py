f93c1ab02cc6fcfe1e21d38c545e60c4bfb772a8
vipabh2 <a8919832@googlemail.com>
2024-12-10 12:30:16 +0300

    Update الخطوط.py

--- VIPABH/plugins/الخطوط.py
+++ VIPABH/plugins/الخطوط.py
@@ -1,13 +1,11 @@
-# Reda - Hussein
-# © JoKeRUB Team 2023
-# ها شعدك داخل ع الملف تريد تخمط ؟ ابو زربة لهل درجة فاشل  
+
 from telethon import events
-from JoKeRUB import l313l
+from VIPABH import ABH
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 from ..core.managers import edit_delete
 from telethon import functions
 from telethon.errors.rpcerrorlist import MessageIdInvalidError
-@l313l.on(admin_cmd(pattern="(خط الغامق|خط غامق)"))
+@ABH.on(admin_cmd(pattern="(خط الغامق|خط غامق)"))
 async def btext(event):
     isbold = gvarstatus("bold")
     if not isbold:
@@ -20,7 +18,7 @@ async def btext(event):
         await edit_delete(event, "**᯽︙ تم اطفاء خط الغامق بنجاح ✓ **")
         return
 
-@l313l.on(admin_cmd(pattern="(خط المشطوب|خط مشطوب)"))
+@ABH.on(admin_cmd(pattern="(خط المشطوب|خط مشطوب)"))
 async def btext(event):
     istshwesh = gvarstatus("tshwesh")
     if not istshwesh:
@@ -33,7 +31,7 @@ async def btext(event):
         await edit_delete(event, "**᯽︙ تم اطفاء خط المشطوب بنجاح ✓ **")
         return
 
-@l313l.on(admin_cmd(pattern="(خط رمز|خط الرمز)"))
+@ABH.on(admin_cmd(pattern="(خط رمز|خط الرمز)"))
 async def btext(event):
     isramz = gvarstatus("ramz")
     if not isramz:
@@ -46,7 +44,7 @@ async def btext(event):
         await edit_delete(event, "**᯽︙ تم اطفاء خط الرمز بنجاح ✓ **")
         return
 
-@l313l.on(admin_cmd(pattern="(خط الجوكر|خط جوكر)"))
+@ABH.on(admin_cmd(pattern="(خط الجوكر|خط جوكر)"))
 async def Aljoker(event):
     Aljoker = gvarstatus("joker")
     if not Aljoker:
@@ -59,7 +57,7 @@ async def Aljoker(event):
         await edit_delete(event, "**᯽︙ تم اطفاء خط الجوكر بنجاح ✓ **")
         return
 
-@l313l.on(events.NewMessage(outgoing=True))
+@ABH.on(events.NewMessage(outgoing=True))
 async def reda(event):
     if event.message.text and not event.message.media and event.message.text.count(".") != 1 and event.message.text.count("@") != 1:
         isbold = gvarstatus("bold")
