eeb72786bcf954fe896003aa7e93d0e71787f119
vipabh2 <a8919832@googlemail.com>
2024-12-10 04:18:16 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,1 +1,15 @@
-
+from sample_config import Config
+class Development(Config):
+    APP_ID = 
+    API_HASH = ""
+    ALIVE_NAME = ""
+    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
+    DB_URI = ""
+    STRING_SESSION = ""
+    TG_BOT_TOKEN = ""
+    # command handler
+    COMMAND_HAND_LER = "."
+    # sudo enter the id of sudo users userid's in that array
+    SUDO_USERS = []
+    # command hanler for sudo
+    SUDO_COMMAND_HAND_LER = "."
