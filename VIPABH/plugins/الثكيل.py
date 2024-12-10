b4682e84302c8e975a7b60de60623924bc963aa1
vipabh2 <a8919832@googlemail.com>
2024-12-10 12:26:14 +0300

    Update الثكيل.py

--- VIPABH/plugins/الثكيل.py
+++ VIPABH/plugins/الثكيل.py
@@ -1,12 +1,12 @@
 
 import asyncio
 from telethon import events
-from JoKeRUB import l313l
+from VIPABH import ABH
 hussein_enabled = False
 aljoker_enabled = False
 JOKER_ID = {}
 
-@l313l.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+@ABH.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def mark_as_read(event):
     global aljoker_enabled, JOKER_ID
     sender_id = event.sender_id
@@ -16,13 +16,13 @@ async def mark_as_read(event):
             await asyncio.sleep(joker_time)
         await event.mark_read()
 
-@l313l.on(events.NewMessage(outgoing=True, pattern=r'^\.التكبر تعطيل$'))
+@ABH.on(events.NewMessage(outgoing=True, pattern=r'^\.التكبر تعطيل$'))
 async def Hussein(event):
     global aljoker_enabled
     aljoker_enabled = False
     await event.edit('**᯽︙ تم تعطيل امر التكبر بنجاح ✅**')
 
-@l313l.on(events.NewMessage(outgoing=True, pattern=r'^\.التكبر (\d+) (\d+)$'))
+@ABH.on(events.NewMessage(outgoing=True, pattern=r'^\.التكبر (\d+) (\d+)$'))
 async def Hussein(event):
     global aljoker_enabled, JOKER_ID
     joker_time = int(event.pattern_match.group(1))
@@ -31,20 +31,20 @@ async def Hussein(event):
     aljoker_enabled = True
     await event.edit(f'**᯽︙ تم تفعيل امر التكبر بنجاح مع  {joker_time} ثانية للمستخدم {user_id}**')
 
-@l313l.on(events.NewMessage(outgoing=True, pattern=r'^\.مود التكبر تعطيل$'))
+@ABH.on(events.NewMessage(outgoing=True, pattern=r'^\.مود التكبر تعطيل$'))
 async def Hussein(event):
     global hussein_enabled
     hussein_enabled = False
     await event.edit('**᯽︙ تم تعطيل امر التكبر على الجميع بنجاح ✅**')
     
-@l313l.on(admin_cmd(pattern=f"مود التكبر (\d+)"))
+@ABH.on(admin_cmd(pattern=f"مود التكبر (\d+)"))
 async def Hussein(event):
     global hussein_enabled, hussein_time
     hussein_time = int(event.pattern_match.group(1))
     hussein_enabled = True
     await event.edit(f'**᯽︙ تم تفعيل امر التكبر بنجاح مع  {hussein_time} ثانية**')
 
-@l313l.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+@ABH.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def Hussein(event):
     global hussein_enabled, hussein_time
     if hussein_enabled:
