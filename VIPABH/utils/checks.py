86ca2fc0a9e889a5b297efe84f3eb0eb470668c7
vipabh2 <a8919832@googlemail.com>
2024-12-10 03:40:33 +0300

    Update checks.py

--- VIPABH/utils/checks.py
+++ VIPABH/utils/checks.py
@@ -6,11 +6,11 @@ LOGS = logging.getLogger(__name__)
 
 
 # Admin checker by uniborg
-async def is_admin(l313l, chat_id, userid):
+async def is_admin(ABH, chat_id, userid):
     if not str(chat_id).startswith("-100"):
         return False
     try:
-        req_jo = await l313l.get_permissions(chat_id, userid)
+        req_jo = await ABH.get_permissions(chat_id, userid)
         chat_participant = req_jo.participant
         if isinstance(
             chat_participant, (ChannelParticipantCreator, ChannelParticipantAdmin)
