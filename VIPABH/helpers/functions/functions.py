18bbefcff07f256f54d7a24cb90111b615b07e15
vipabh2 <a8919832@googlemail.com>
2024-12-10 15:10:03 +0300

    Update functions.py

--- VIPABH/helpers/functions/functions.py
+++ VIPABH/helpers/functions/functions.py
@@ -364,7 +364,7 @@ def higlighted_text(
         else (int(1024 * raw_width / raw_height), 1024)
     )
     if font_name is None:
-        font_name = "JoKeRUB/helpers/styles/impact.ttf"
+        font_name = "VIPABH/helpers/styles/impact.ttf"
     font = ImageFont.truetype(font_name, font_size)
     extra_width, extra_height = position
     # get text size
