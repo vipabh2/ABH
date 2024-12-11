0add79e954ebd3f8f937ae1502a03c0279cb0e1a
vipabh2 <a8919832@googlemail.com>
2024-12-11 23:26:14 +0300

    Update اوامري.py

--- VIPABH/plugins/اوامري.py
+++ VIPABH/plugins/اوامري.py
@@ -10,7 +10,7 @@ from ..core import check_owner
 from ..Config import Config
 
 JEP_IC = "https://telegra.ph/file/762989c65df81fc2e96d7.jpg"
-ROE = "**♰ هـذه هي قائمة اوامـر سـورس الجوكر ♰**"
+ROE = "**♰ هـذه هي قائمة اوامـر سـورس ABH ♰**"
 
 if Config.TG_BOT_USERNAME is not None and tgbot is not None:
 
