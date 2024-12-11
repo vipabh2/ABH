3800a974884d8456233137985c2145365afcc63c
vipabh2 <a8919832@googlemail.com>
2024-12-11 22:27:44 +0300

    Update معلومات الكروب.py

--- VIPABH/plugins/معلومات الكروب.py
+++ VIPABH/plugins/معلومات الكروب.py
@@ -186,7 +186,7 @@ async def get_users(show):
             "{tr}المعلومـات <username/userid>",
             "{tr}المعلومـات <in group where you need>",
         ],
-        "examples": "{tr}معلومات @jepthon",
+        "examples": "{tr}معلومات ",
     },
 )
 async def info(event):
