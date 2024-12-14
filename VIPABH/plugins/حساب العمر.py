86a90875de041fa97d5dbde4cc1a709b0ee232fb
vipabh2 <a8919832@googlemail.com>
2024-12-15 02:34:16 +0300

    Update حساب العمر.py

--- VIPABH/plugins/حساب العمر.py
+++ VIPABH/plugins/حساب العمر.py
@@ -1,15 +1,16 @@
-#ZedThon
-
 from datetime import datetime
-
 from ..core.managers import edit_delete, edit_or_reply
-
 
 @ABH.ar_cmd(pattern="مواليد(?:\s|$)([\s\S]*)")
 async def _(event):
-    yar = event.text[12:]
-    if not yar:
-       return await edit_or_reply(event, "**✾╎استخـدم الامـر كالتالـي .. حساب العمر + السنـه**")
-    YearNow = datetime.now().year
-    MyAge = YearNow - yar
-    await edit_or_reply(e, "**🚹╎عمرك هـو :**  {}".format(MyAge))
+    yar = event.text[12:].strip()  # استبعاد المسافات
+    if not yar or not yar.isdigit():
+        return await edit_or_reply(event, "**✾╎استخـدم الامر كالتالي: حساب العمر + السنة**")
+
+    YearNow = datetime.now().year  # الحصول على السنة الحالية
+    try:
+        birth_year = int(yar)  # تحويل السنة المدخلة إلى عدد صحيح
+        MyAge = YearNow - birth_year  # حساب العمر
+        await edit_or_reply(event, f"**🚹╎عمرك هـو : {MyAge} سنة**")
+    except ValueError:
+        return await edit_or_reply(event, "**✾╎حدث خطأ، تأكد من إدخال سنة صحيحة.**")
