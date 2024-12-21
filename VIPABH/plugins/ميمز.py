47c81f7982e48573e556f56f2ee68061f9076ef9
vipabh2 <a8919832@googlemail.com>
2024-12-22 02:06:18 +0300

    Update ميمز.py

--- VIPABH/plugins/ميمز.py
+++ VIPABH/plugins/ميمز.py
@@ -9,14 +9,18 @@ from telethon import events
 from ..sql_helper.memes_sql import get_link, add_link, delete_link, BASE, SESSION, AljokerLink
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from VIPABH import ABH
+from abh import __init__.py
 from ..helpers.utils import reply_id
 plugin_category = "tools"
 aljoker_links = {}
 
+BOTLOG_CHATID = Config.BOTLOG_CHATID
+
 @ABH.on(admin_cmd(outgoing=True, pattern="لطمية$"))
 async def jepThe(theme):
     try:
-        rl = random.randint(19, 170)        
+        # rl = random.randint(19, 170)        
+        rl = random.randint(999, 988)        
         url = f"https://t.me/x04ou/{rl}"
         await theme.client.send_file(
             theme.chat_id, 
