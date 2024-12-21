b2f8958528805e57b8b52454a14a6fb1e985d3a9
vipabh2 <a8919832@googlemail.com>
2024-12-22 01:49:18 +0300

    Update ميمز.py

--- VIPABH/plugins/ميمز.py
+++ VIPABH/plugins/ميمز.py
@@ -18,13 +18,13 @@ async def jepThe(theme):
     try:
         rl = random.randint(19, 170)        
         url = f"https://t.me/x04ou/{rl}"
-        await theme.ABH.on.send_file(
+        await theme.client.send_file(
             theme.chat_id, 
             url, 
             caption="᯽︙  اذكر القائم", 
             parse_mode="html"
-        )
-        await theme.@ABH.on.send_message(
+        )        
+        await theme.client.send_message(
             BOTLOG_CHATID,
             f"᯽︙ الـتثبيت\
                 \n **᯽︙ تـم بـنجـاح الـتثبيت فـي الدردشـة**\
@@ -35,7 +35,8 @@ async def jepThe(theme):
     except Exception as e:
         error_message = str(e)
         message_link = f"https://t.me/c/{theme.chat_id}/{theme.id}"  # رابط الرسالة في الدردشة
-        await theme.ABH.on.send_message(
+        
+        await theme.client.send_message(
             BOTLOG_CHATID,
             f"❌ **حدث خطأ أثناء إرسال اللطمية**\n"
             f"رابط الرسالة: {message_link}\n"
@@ -43,3 +44,4 @@ async def jepThe(theme):
             f"رابط اللطمية الخاطئة: {url}"
         )
         await theme.edit(" حدث خطأ أثناء إرسال اللطمية.")
+
