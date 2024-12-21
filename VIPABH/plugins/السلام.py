1767a720558aa8fbb3fc74289bc443ed52ddabf3
vipabh2 <a8919832@googlemail.com>
2024-12-21 14:58:32 +0300

    Create السلام.py

---
+++ VIPABH/plugins/السلام.py
@@ -0,0 +1,31 @@
+from telethon import events, 
+from VIPABH import ABH
+from ..core.managers import edit_or_reply
+
+auto_replay = False 
+@ABH.ar_cmd(pattern="السلام تعطيل")
+async def auto_replay_off(event):
+    global auto_replay
+    auto_replay = False  
+    await event.reply("تم تعطيل الرد التلقائي على السلام.")
+
+@ABH.ar_cmd(pattern="السلام تفعيل")
+async def auto_replay_on(event):
+    global auto_replay
+    auto_replay = True 
+    await event.reply("تم تفعيل الرد التلقائي على السلام.")
+
+@ABH.ar_cmd(
+    incoming=True,
+    func=lambda e: e.text and (e.text.strip().lower() == "السلام عليكم" or e.text.strip().lower() == "سلام عليكم"),
+    edited=False
+)
+async def reply_salam(event):
+    if event.sender.id in banned_users:
+        return 
+
+    if not auto_replay:
+        return 
+    # username = event.sender.username if event.sender.username else "لا يوجد اسم مستخدم"
+    await event.reply("عليكم السلام")
+    # await event.reply(f"عليكم السلام {username}")
