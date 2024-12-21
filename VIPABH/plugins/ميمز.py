619086ee0ba9699d76a40c85b88032dcc7d0565e
vipabh2 <a8919832@googlemail.com>
2024-12-22 01:46:08 +0300

    Update ميمز.py

--- VIPABH/plugins/ميمز.py
+++ VIPABH/plugins/ميمز.py
@@ -15,7 +15,31 @@ aljoker_links = {}
 
 @ABH.on(admin_cmd(outgoing=True, pattern="لطمية$"))
 async def jepThe(theme):
-    rl = random.randint(19, 170)
-    url = f"https://t.me/x04ou/{rl}"
-    await theme.client.send_file(theme.chat_id, url, caption="᯽︙  اذكر القائم ", parse_mode="html")
-    await theme.delete()
+    try:
+        rl = random.randint(19, 170)        
+        url = f"https://t.me/x04ou/{rl}"
+        await theme.ABH.on.send_file(
+            theme.chat_id, 
+            url, 
+            caption="᯽︙  اذكر القائم", 
+            parse_mode="html"
+        )
+        await theme.@ABH.on.send_message(
+            BOTLOG_CHATID,
+            f"᯽︙ الـتثبيت\
+                \n **᯽︙ تـم بـنجـاح الـتثبيت فـي الدردشـة**\
+                \nالـدردشـة: {theme.chat.title}(`{theme.chat_id}`)\
+                \nالـتثبيت: {is_silent}",
+        )
+        await theme.delete()
+    except Exception as e:
+        error_message = str(e)
+        message_link = f"https://t.me/c/{theme.chat_id}/{theme.id}"  # رابط الرسالة في الدردشة
+        await theme.ABH.on.send_message(
+            BOTLOG_CHATID,
+            f"❌ **حدث خطأ أثناء إرسال اللطمية**\n"
+            f"رابط الرسالة: {message_link}\n"
+            f"تفاصيل الخطأ: {error_message}"
+            f"رابط اللطمية الخاطئة: {url}"
+        )
+        await theme.edit(" حدث خطأ أثناء إرسال اللطمية.")
