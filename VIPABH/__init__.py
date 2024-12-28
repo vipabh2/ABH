92272381416912d97dabd9be3f3aa4d67b17ab09
vipabh2 <a8919832@googlemail.com>
2024-12-28 20:02:48 +0300

    Update __init__.py

--- VIPABH/__init__.py
+++ VIPABH/__init__.py
@@ -48,15 +48,15 @@ if Config.PM_LOGGER_GROUP_ID == 0:
         Config.PM_LOGGER_GROUP_ID = int(gvarstatus("PM_LOGGER_GROUP_ID"))
 elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
     Config.PM_LOGGER_GROUP_ID = int("-" + str(Config.PM_LOGGER_GROUP_ID))
-# try:
-#     if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
-#         HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
-#             Config.HEROKU_APP_NAME
-#         ]
-#     else:
-#         HEROKU_APP = None
-# except Exception:
-#     HEROKU_APP = None
+try:
+    if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
+        HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
+            Config.HEROKU_APP_NAME
+        ]
+    else:
+        HEROKU_APP = None
+except Exception:
+    HEROKU_APP = None
 
 
 # Global Configiables
