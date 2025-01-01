eaf88c96a2a09d302a6b64aff2db67006564cd0f
vipabh2 <a8919832@googlemail.com>
2025-01-01 20:59:48 +0300

    Update startup.py

--- VIPABH/utils/startup.py
+++ VIPABH/utils/startup.py
@@ -295,7 +295,7 @@ async def verifyLoggerGroup():
             )
     else:
         descript = "- عزيزي المستخدم هذه هي مجموعه الاشعارات يرجى عدم حذفها  - "
-        photobt = await ABH.upload_file(file="ABH/razan/resources/start/abh2.JPEG")
+        photobt = await ABH.upload_file(file="ABH/razan/resources/start/abh2.jpg")
         botlog_group_id = await aljoker_the_best(ABH, "مجموعة أشعارات ABH")
         if botlog_group_id:
             addgvar("PRIVATE_GROUP_BOT_API_ID", botlog_group_id)
@@ -309,7 +309,7 @@ async def verifyLoggerGroup():
         flag = True
     if PM_LOGGER_GROUP_ID == -100:
         descript = "᯽︙ وظيفه الكروب يحفظ رسائل الخاص اذا ما تريد الامر احذف الكروب نهائي \n  - "
-        photobt = await ABH.upload_file(file="ABH/razan/resources/start/abh.JPEG")
+        photobt = await ABH.upload_file(file="ABH/razan/resources/start/abh.jpg")
         pm_logger_group_id = await aljoker_the_best(ABH, "مجموعة التخزين")
         if pm_logger_group_id:
             addgvar("PM_LOGGER_GROUP_ID", pm_logger_group_id)
