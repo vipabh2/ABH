0b9e9ec033195507e854debc1ffdf0fbd69bfcc4
vipabh2 <a8919832@googlemail.com>
2025-02-06 13:48:33 +0300

    Update session.py

--- VIPABH/core/session.py
+++ VIPABH/core/session.py
@@ -32,7 +32,7 @@ except Exception as e:
     sys.exit()
 try:
     ABH.tgbot = tgbot = HuReClient(
-        session="arTgbot",
+        session="ABH",
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
         loop=loop,
