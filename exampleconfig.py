eabc15a6a67351604d01f20f285e0ca3f590728b
vipabh2 <a8919832@googlemail.com>
2025-03-03 02:20:53 +0300

    Add files via upload

---
+++ exampleconfig.py
@@ -0,0 +1,11 @@
+from sample_config import Config
+class Development(Config):
+    APP_ID = 
+    API_HASH = ""
+    ALIVE_NAME = ""
+    DB_URI = ""
+    STRING_SESSION = ""
+    TG_BOT_TOKEN = ""
+    COMMAND_HAND_LER = "."
+    SUDO_USERS = []
+    SUDO_COMMAND_HAND_LER = "."
