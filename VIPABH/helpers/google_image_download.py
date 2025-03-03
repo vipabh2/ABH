9340a656dd92682ae722e1a3ab1a376de5bdf991
vipabh2 <a8919832@googlemail.com>
2025-03-03 17:57:24 +0300

    Update google_image_download.py

--- VIPABH/helpers/google_image_download.py
+++ VIPABH/helpers/google_image_download.py
@@ -1,19 +1,4 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# CatUserBot #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# Copyright (C) 2020-2023 by TgCatUB@Github.
-
-# This file is part of: https://github.com/TgCatUB/catuserbot
-# and is released under the "GNU v3.0 License Agreement".
-
-# Please see: https://github.com/TgCatUB/catuserbot/blob/master/LICENSE
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
-#!/usr/bin/env python
-# In[ ]:
-#  coding: utf-8
-
-###### Searching and Downloading Google Images to the local disk ######
-
-# Import Libraries
+import Libraries
 import argparse
 import codecs
 import datetime
