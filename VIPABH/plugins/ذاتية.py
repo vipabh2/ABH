2bc6b951da6af90e453c8886767515c8fbff29da
vipabh2 <a8919832@googlemail.com>
2025-03-03 16:50:26 +0300

    Add files via upload

---
+++ VIPABH/plugins/ذاتية.py
@@ -0,0 +1,71 @@
+from VIPABH import ABH
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+import os
+import datetime
+from telethon import events
+from VIPABH import *
+Aljoker_Asbo3 = {
+    'Monday': 'الاثنين',
+    'Tuesday': 'الثلاثاء',
+    'Wednesday': 'الأربعاء',
+    'Thursday': 'الخميس',
+    'Friday': 'الجمعة',
+    'Saturday': 'السبت',
+    'Sunday': 'الأحد'
+}
+@ABH.on(admin_cmd(pattern="(جلب الصورة|جلب الصوره|ذاتيه|ذاتية)"))
+async def dato(event):
+    if not event.is_reply:
+        return await event.edit("..")
+    lMl10l = await event.get_reply_message()
+    pic = await lMl10l.download_media()
+    await bot.send_file(
+        "me",
+        pic,
+        caption=f"""
+- تـم حفظ الصـورة بنجـاح ✓ 
+- 
+  """,
+    )
+    await event.delete()
+@ABH.on(admin_cmd(pattern="(الذاتية تشغيل|ذاتية تشغيل)"))
+async def reda(event):
+    if gvarstatus ("savepicforme"):
+        return await edit_delete(event, "**᯽︙حفظ الذاتيات مفعل وليس بحاجة للتفعيل مجدداً **")
+    else:
+        addgvar("savepicforme", "reda")
+        await edit_delete(event, "**᯽︙تم تفعيل ميزة حفظ الذاتيات بنجاح ✓**")
+ 
+@ABH.on(admin_cmd(pattern="(الذاتية تعطيل|ذاتية تعطيل)"))
+async def Reda_Is_Here(event):
+    if gvarstatus ("savepicforme"):
+        delgvar("savepicforme")
+        return await edit_delete(event, "**᯽︙تم تعطيل حفظت الذاتيات بنجاح ✓**")
+    else:
+        await edit_delete(event, "**᯽︙انت لم تفعل حفظ الذاتيات لتعطيلها!**")
+def joker_unread_media(message):
+    return message.media_unread and (message.photo or message.video)
+async def Hussein(event, caption):
+    media = await event.download_media()
+    sender = await event.get_sender()
+    sender_id = event.sender_id
+    lMl10l_date = event.date.strftime("%Y-%m-%d")
+    lMl10l_day = Aljoker_Asbo3[event.date.strftime("%A")]
+    await bot.send_file(
+        "me",
+        media,
+        caption=caption.format(sender.first_name, sender_id, lMl10l_date, lMl10l_day),
+        parse_mode="markdown"
+    )
+    os.remove(media)
+@ABH.on(events.NewMessage(func=lambda e: e.is_private and joker_unread_media(e) and e.sender_id != bot.uid))
+async def Reda(event):
+    if gvarstatus("savepicforme"):
+        caption = """**
+           ♡ تم حفظ الذاتية بنجاح ✓
+♡ أسم المرسل : [{0}](tg://user?id={1})
+♡  تاريخ الذاتية : `{2}`
+♡  أرسلت في يوم `{3}`
+       ♡    ABH    ♡
+        **"""
+        await Hussein(event, caption)
